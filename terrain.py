ee430f22582a1bc7ebaba20442196989fa8a412f
shirleyhappyv-ux <shirley.happyv@gmail.com>
2026-03-09 17:52:10 +0800

    Update terrain.py

--- terrain.py
+++ terrain.py
@@ -1,18 +1,16 @@
 import numpy as np
 
-def generate_terrain():
+def generate_terrain(size=60):
 
-    size = 80
-
-    x = np.linspace(-10,10,size)
-    y = np.linspace(-10,10,size)
+    x = np.linspace(-5,5,size)
+    y = np.linspace(-5,5,size)
 
     X,Y = np.meshgrid(x,y)
 
     Z = (
-        5*np.sin(np.sqrt(X**2+Y**2))
-        +2*np.cos(Y/2)
-        +np.random.rand(size,size)
+        np.sin(X)*np.cos(Y)*30
+        + np.random.randn(size,size)*2
+        + 50
     )
 
     return Z
